4a0e20099e7e76de54ad0eddea32f95fcd8a60b3
rizinaa99 <rizinakhatun@icloud.com>
2021-12-15 14:32:06 +0000

    completed 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,17 @@
 class DockingStation
-   def release_bike
+  def release_bike
      Bike.new
-   end
-
-   def dock(bike)
-     @bike = bike
-   end
-attr_reader :bike
+  end
+  
+  # def dock(bike)
+  #   @bike = bike
+  # end
+  # def release_bike
+  #   @bike
+  # end
+  def release_bike
+    fail 'No bikes available' unless @bike
+    @bike
+  end
+  # attr_reader :bike
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,10 +10,19 @@ describe DockingStation do
   # it 'docks something' do
     # expect(subject.dock(bike)).to eq bike
   # end
+  # it 'returns docked bikes' do
+    # bike = Bike.new
+    # subject.dock(bike)
+    # expect(subject.bike).to eq bike
+  # end
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+    # it 'releases a bike' do
+    # bike = Bike.new
+    # subject.dock(bike)
+    # expect(subject.release_bike).to eq bike
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 end
